0ac496d3225ef107fab1559be3d9c9ea171f956d
toasam <dmstoa0322@naver.com>
2022-05-02 00:53:07 +0900

    #2.1~#2.2
    .

--- app.js
+++ app.js
@@ -6,6 +6,14 @@
     
     // canvas위에 마우스를 두면 그걸 감지하는 코드
     const canvas = document.getElementById("jsCanvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = 700;
+    canvas.height = 700;
+
+    // strokeStyle -> 색상이나 스타일을 라인에서 사용
+    ctx.strokeStyle = "#2c2c2c";
+    ctx.lineWidth = 2.5;
 
     let painting = false;
 
@@ -13,12 +21,24 @@
         painting = false;
     }
 
+    function startPainting(){
+        painting = true;
+    }
 
     // 5라인 이어서 -> 그럼 이벤트 안에서 offsetX와 offesetY를 사용
+    // onMouseMove 여기에서 모든 움직임을 감지, 라인을 만들어야 함
+    // path : 기본적인 선(line) ctx=context
+    // lineTo()와 stroke()는  마우스를 움직이는 내내 발생함!!
     function onMouseMove(event){
         const x = event.offsetX;
         const y = event.offsetY;
-        console.log(x,y);
+        if(!painting){ // 경로를 만든다.
+            ctx.beginPath(); // 경로생성
+            ctx.moveTo(x,y); // 선 시작 좌표
+            }else{ // 그린다.
+                ctx.lineTo(x,y); // 선 끝 좌표
+                ctx.stroke(); // 선 그리기
+            }
     }
 
     //event를 줘야 색상을 볼 수 있음.
@@ -41,8 +61,14 @@
     // mouseleave painting 하다가 캔버스를 벗어났을 때  
     if(canvas){
         canvas.addEventListener("mousemove", onMouseMove);
-        canvas.addEventListener("mousedown", onMouseDown);
+        canvas.addEventListener("mousedown", startPainting);
     // onMouseUp을 stopPainting 안 바꾼 이유 : 나중에 실제로 그리는 line이 필요
-        canvas.addEventListener("mouseup", onMouseUp);
+        canvas.addEventListener("mouseup", stopPainting);
         canvas.addEventListener("mouseleave",stopPainting);
     }
+
+
+    // Context 요소 안에서 픽셀에 접근하는 방법
+    // 여기서 context는 canvas안에서 픽셀을 다루는 것
+    // canvas는 context를 갖고 있는 html의 요소
+    // 그 요소 안에서 픽셀을 다루는 것
